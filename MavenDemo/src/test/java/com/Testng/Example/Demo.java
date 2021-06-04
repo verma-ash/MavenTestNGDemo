f40e10933f9c391edd847414fbd51479255ca0b9
Áshish <ashishkumarverma33338@gmail.com>
2021-06-04 15:14:54 +0530

    comment for  04-06-2021

--- MavenDemo/src/test/java/com/Testng/Example/Demo.java
+++ MavenDemo/src/test/java/com/Testng/Example/Demo.java
@@ -1,12 +1,31 @@
 package com.Testng.Example;
 
-import org.testng.annotations.Test;
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo {
-	
-	@Test
-	public void work() {
-		System.out.println("Hello World !");
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println(driver instanceof TakesScreenshot);
+		
+		driver.get("https://www.google.com");
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(scrFile, new File("C:\\Users\\Ashish\\Desktop\\New Text Document.jpg"));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 	}
 
 }
